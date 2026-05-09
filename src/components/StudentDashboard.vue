449238badc5817e3aee4114c4303842068efba34
Abdullah Abuat Lacsaman <lacsaman.aa60@s.msumain.edu.ph>
2026-05-09 23:05:36 +0800

    Add files via upload

---
+++ src/components/StudentDashboard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ref, computed } from 'vue'
+  import RoadmapTab from './Student/RoadmapTab.vue'
+  import SyllabusTab from './Student/SyllabusTab.vue'
+  import TutorialTab from './Student/TutorialTab.vue'
+  import EnrolledTab from './Student/EnrolledTab.vue'
+  import ProfessorTab from './Student/ProfessorTab.vue'
+  import AnnouncementTab from './Student/AnnouncementTab.vue'
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
