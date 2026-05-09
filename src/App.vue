7bc991da41b5c0a3b7c01133c0f7dbcfad7f5608
Abdullah Abuat Lacsaman <lacsaman.aa60@s.msumain.edu.ph>
2026-05-09 23:01:48 +0800

    Refactor App.vue to include components and update template
    Import Login, StudentDashboard, and ProfessorDashboard components. Update template and style sections.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import Login from './components/Login.vue'
+import StudentDashboard from './components/StudentDashboard.vue'
+import ProfessorDashboard from './components/ProfessorDashboard.vue'
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+ 
 </template>
 
-<style scoped></style>
+<style>
+  
+</style>
